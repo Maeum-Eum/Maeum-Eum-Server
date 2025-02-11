549fbca41a27b3bbe088417e148179f21a41f00a
oohyj <hyj0660@gmail.com>
2025-02-11 14:54:33 +0900

    chore : 프로젝트 세팅

---
+++ Maeum-Eum/src/main/java/com/five/Maeum_Eum/MaeumEumApplication.java
@@ -0,0 +1,13 @@
+package com.five.Maeum_Eum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeumEumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaeumEumApplication.class, args);
+	}
+
+}
---
+++ Maeum-Eum/src/test/java/com/five/Maeum_Eum/MaeumEumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.five.Maeum_Eum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeumEumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
