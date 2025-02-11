b24b2249502394e291d5e06833d4e0ff1d56d157
pysunn <kminseok14@ajou.ac.kr>
2025-02-11 16:03:51 +0900

    [ADD] CICD 관련파일
    appspec.yml : shell script 실행용
    start.sh, stop.sh : codedeploy 배포 후 실행

---
+++ Maeum-Eum/scripts/start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=/home/ec2-user/Maeum-Eum
+JAR="$PROJECT_ROOT/application.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+NOW=$(date +%c)
+
+# build 파일 복사
+echo "[$NOW] > $JAR 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR
+
+# jar 파일 실행
+echo "[$NOW] > $JAR 파일 실행" >> $DEPLOY_LOG
+nohup sudo java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+
+PID=$(pgrep -f $JAR)
+echo "$NOW > 현재 실행된 프로세스 아이디 $PID 입니다." >> $DEPLOY_LOG
---
+++ Maeum-Eum/scripts/stop.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=/home/ec2-user/Maeum-Eum
+JAR="$PROJECT_ROOT/application.jar"
+
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+NOW=$(date +%c)
+
+# 현재 구동 중인 애플리케이션 pid 확인
+PID=$(pgrep -f $JAR | head -n 1)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z "$PID" ]; then
+  echo "$NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  # -15 : 정상 종료
+  sudo kill -15 "$PID"
+fi
