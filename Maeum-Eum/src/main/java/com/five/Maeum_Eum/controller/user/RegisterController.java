87c15d94c0e50f224461cfd9d27d8508f4f1249d
jjjjjk12 <jjjjjk12@naver.com>
2025-02-13 21:45:30 +0900

    feat: 아이디 중복 검사

--- Maeum-Eum/src/main/java/com/five/Maeum_Eum/controller/user/RegisterController.java
+++ Maeum-Eum/src/main/java/com/five/Maeum_Eum/controller/user/RegisterController.java
@@ -1,5 +1,6 @@
 package com.five.Maeum_Eum.controller.user;
 
+import com.five.Maeum_Eum.dto.user.login.request.UserLoginDTO;
 import com.five.Maeum_Eum.dto.user.register.request.CaregiverRegiDTO;
 import com.five.Maeum_Eum.dto.user.register.request.ManagerRegiDTO;
 import com.five.Maeum_Eum.exception.ErrorResponse;
@@ -44,5 +45,23 @@ public class RegisterController {
                         .status(HttpStatus.BAD_REQUEST)
                         .message("잘못된 입력이 있습니다. 입력을 확인해주세요")
                         .build());
+    }
+
+    @PostMapping("/validateID")
+    public ResponseEntity<Object> validateDuple(@RequestBody UserLoginDTO dto) {
+
+        if (registerService.validateDuplicateID(dto.getId())) {
+            return ResponseEntity
+                    .status(400)
+                    .body(ErrorResponse.builder()
+                            .code("InvalidInput")
+                            .status(HttpStatus.BAD_REQUEST)
+                            .message("중복된 아이디입니다")
+                            .build());
+        }
+
+        else {
+            return ResponseEntity.status(200).body(dto);
+        }
     }
 }
