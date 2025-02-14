afe5e0a0baf43f436f9d6b311218a84711ae3411
pysunn <kminseok14@ajou.ac.kr>
2025-02-15 01:29:27 +0900

    CertificateRepository 삭제 (엔티티 아님)

--- Maeum-Eum/src/main/java/com/five/Maeum_Eum/repository/caregiver/CertificateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.five.Maeum_Eum.repository.caregiver;
-
-import com.five.Maeum_Eum.entity.user.caregiver.Certificate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository<Certificate , Long> {
-}
