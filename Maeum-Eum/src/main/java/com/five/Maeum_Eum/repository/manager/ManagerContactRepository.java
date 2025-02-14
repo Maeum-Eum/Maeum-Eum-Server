e2e643257af36d50a09cfe21ad5738839a56d52b
oohyj <hyj0660@gmail.com>
2025-02-14 13:08:12 +0900

    fix : git conflict resolve

--- Maeum-Eum/src/main/java/com/five/Maeum_Eum/repository/manager/ManagerContactRepository.java
+++ Maeum-Eum/src/main/java/com/five/Maeum_Eum/repository/manager/ManagerContactRepository.java
@@ -1,7 +1,10 @@
 package com.five.Maeum_Eum.repository.manager;
 
+import com.five.Maeum_Eum.entity.user.manager.ManagerContact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ManagerContactRepository extends JpaRepository<ManagerContactRepository, Long> {
+@Repository
+public interface ManagerContactRepository extends JpaRepository<ManagerContact, Long> {
 }
 
