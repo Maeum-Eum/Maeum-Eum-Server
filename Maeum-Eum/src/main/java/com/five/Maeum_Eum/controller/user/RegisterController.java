64113b7dac6deee673d8492683aee9dbe0d71fa3
jjjjjk12 <jjjjjk12@naver.com>
2025-02-17 11:15:07 +0900

    fix: 요양복지사 회원가입 api 수정, 프로필 사진 추가

--- Maeum-Eum/src/main/java/com/five/Maeum_Eum/controller/user/RegisterController.java
+++ Maeum-Eum/src/main/java/com/five/Maeum_Eum/controller/user/RegisterController.java
@@ -4,15 +4,17 @@ import com.five.Maeum_Eum.dto.user.login.request.UserLoginDTO;
 import com.five.Maeum_Eum.dto.user.manager.response.ManagerBasicDto;
 import com.five.Maeum_Eum.dto.user.register.request.CaregiverRegiDTO;
 import com.five.Maeum_Eum.dto.user.register.request.ManagerRegiDTO;
+import com.five.Maeum_Eum.entity.user.caregiver.Caregiver;
+import com.five.Maeum_Eum.exception.CustomException;
+import com.five.Maeum_Eum.exception.ErrorCode;
 import com.five.Maeum_Eum.exception.ErrorResponse;
 import com.five.Maeum_Eum.service.user.RegisterService;
+import com.five.Maeum_Eum.service.user.caregiver.CaregiverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisterController {
 
     private final RegisterService registerService;
+    private final CaregiverService caregiverService;
 
     /* 사회복지사 (관리자) 회원가입*/
     @PostMapping("/manager/register")
@@ -39,6 +42,17 @@ public class RegisterController {
                         .status(HttpStatus.BAD_REQUEST)
                         .message("잘못된 입력이 있습니다. 입력을 확인해주세요")
                         .build());
+    }
+
+    @PostMapping("/caregiver/register/{caregiver}")
+    public ResponseEntity<Object> uploadProfile(@PathVariable String caregiver,
+                                                @RequestParam("profileImage") MultipartFile file) throws IllegalAccessException {
+
+        Caregiver caregiver1 = caregiverService.findCaregiverByLoginId(caregiver);
+        if (caregiver1 == null) throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        caregiverService.uploadProfileImage(file, caregiver1);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/validateID")
