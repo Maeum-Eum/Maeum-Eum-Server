818ddff7877a1138f8003749044e3ece16b22c34
pysunn <kminseok14@ajou.ac.kr>
2025-02-19 18:00:33 +0900

    이력서 레벨 연동

---
+++ Maeum-Eum/src/main/java/com/five/Maeum_Eum/controller/work/MealType.java
@@ -0,0 +1,30 @@
+package com.five.Maeum_Eum.controller.work;
+
+import com.five.Maeum_Eum.exception.CustomException;
+import com.five.Maeum_Eum.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public enum MealType {
+    ONE("null",1),
+    TWO("식사 도움",2),
+    THREE("요리 가능",3),
+    FOUR("콧줄 식사",4);
+
+    private final String label;
+    private final int level;
+
+    MealType(String label, int level) {
+        this.label = label;
+        this.level = level;
+    }
+
+    public static MealType fromLabel(String label) {
+        for (MealType place : values()) {
+            if (place.label.equals(label)) {
+                return place;
+            }
+        }
+        throw new CustomException(ErrorCode.INVALID_INPUT, "잘못된 Meal 입력입니다 : " + label + " 필요한 입력 null / 식사 도움 / 요리 가능 / 콧줄 식사");
+    }
+}
